f30361921cb50b3531e425f1091d1e58208a78a8
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-09-21 22:00:35 +0530

    Electron Setup + Go Node Spawning

--- app/main.js
+++ app/main.js
@@ -1,31 +1,52 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('node:path')
+const path = require('node:path');
+const { spawn } = require('node:child_process');
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        preload: path.join(__dirname, 'src/main/preload.js')
-    }
-  })
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'src/main/preload.js')
+        }
+    })
 
-  win.loadFile('src/renderer/index.html')
+    win.loadFile('src/renderer/index.html')
 }
 
 app.whenReady().then(() => {
-ipcMain.handle('ping', () => 'pong')
-  createWindow()
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+    const goNodeArgs = [
+        "-port",
+        "3000",
+        "-nick",
+        "ajmal",
+        "-room",
+        "ajmal",
+    ]
+
+    let goNode = spawn(
+        path.join(app.getAppPath(), "src", "node", "node.exe"),
+        goNodeArgs,
+        {
+            detached: false,
+        },
+    )
+    goNode.stderr.on("data", (d) =>
+        console.log("chat-gonode:", d.toString()),
+    )
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
